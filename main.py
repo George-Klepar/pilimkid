0dcfbd2ac82e024622c0dc814082b78eb91c9904
George-Klepar <artofhard@gmail.com>
2015-05-28 22:11:57 +0200

    pervij suka kommit vtoroj stsuko kommit

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 # Sentence splitting to words function
 def split2words (input_string):
     return input_string.split()
@@ -10,3 +9,27 @@ wordsArray = split2words(sentence)
 # Print result
 for i in wordsArray:
     print(i)
+
+# прiвiт дрiже Дiмi, нiжче бiдiм стрiку рiзбiватi на слiва :)
+
+a = input()
+b = a.split('[., ]+')
+print(b)
+
+# в общем, это почти то же самое, что def split2words, но ты пошел дальше и сделал функцию, я еще далек от изящности кода
+# вот ниже нашел код разбиения слов на слоги, так, для заметки
+'''
+#coding=utf-8
+import re
+vowels = set(u'аеёиоуыэюя')
+sign_chars = set(u'ъь')
+pattern_str = u"(c*[ьъ]?vc+[ьъ](?=v))|(c*[ьъ]?v(?=v|cv))|(c*[ьъ]?vc[ъь]?(?=cv|ccv))|(c*[ьъ]?v[cьъ]*(?=$))"
+pattern = re.compile(pattern_str)
+
+def get_syllables(word):
+    mask = ''.join(['v' if c in vowels else c if c in sign_chars else 'c' for c in word.lower()])
+    return [word[m.start():m.end()] for m in pattern.finditer(mask)]
+
+word = raw_input(u'слово:').decode('utf-8')
+print '-'.join(get_syllables(word))
+'''
