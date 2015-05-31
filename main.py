6c08167f3424c5410474800de78a7fcfcc883616
hal4ed@gmail.com <salmonkoala73>
2015-05-31 23:36:44 +0200

    Test commit

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ wordsArray = s2w(sentence)
 
 for i in wordsArray:
     print(i)
-
+    
+for i in wordsArray:
+    print(i)
 
 # Request to enter sentence to split and write to list
 # stdout function do not carry string to new-line
