72bc79ff3a2ed214c0c357e6827efe24280587ee
Daniel Kowalski <hal4ed@gmail.com>
2015-06-01 10:58:49 +0000

    Major commit Now we have printed result

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import re
-import sys
 
 # Sentence to separate words splitting function
 def s2w (input_string):
@@ -8,39 +7,25 @@ def s2w (input_string):
     return ret
 
 
-# Sentence splitting demo with function "s2w"
+#words to syllables block
 
-sentence = u"Мы - любим кодить...очень: наприпер 'для примера',как типа этого"
-wordsArray = s2w(sentence)
-
-for i in wordsArray:
-    print(i)
-    
-
-# Request to enter sentence to split and write to list
-# stdout function do not carry string to new-line
-
-sys.stdout.write(u'Введите строку: ')
-s = input()
-print (s2w(s))
-
-
-
-
-# в общем, это почти то же самое, что def split2words, но ты пошел дальше и сделал функцию, я еще далек от изящности кода
-# вот ниже нашел код разбиения слов на слоги, так, для заметки
-'''
-#coding=utf-8
-import re
 vowels = set(u'аеёиоуыэюя')
 sign_chars = set(u'ъь')
 pattern_str = u"(c*[ьъ]?vc+[ьъ](?=v))|(c*[ьъ]?v(?=v|cv))|(c*[ьъ]?vc[ъь]?(?=cv|ccv))|(c*[ьъ]?v[cьъ]*(?=$))"
 pattern = re.compile(pattern_str)
 
 def get_syllables(word):
-    mask = ''.join(['v' if c in vowels else c if c in sign_chars else 'c' for c in word.lower()])
-    return [word[m.start():m.end()] for m in pattern.finditer(mask)]
+  mask = ''.join(['v' if c in vowels else c if c in sign_chars else 'c' for c in word.lower()])
+  return [word[m.start():m.end()] for m in pattern.finditer(mask)]
 
-word = raw_input(u'слово:').decode('utf-8')
-print '-'.join(get_syllables(word))
-'''
+
+# Sentence splitting demo with function "s2w"
+
+print(u'Введите строку: ',end="")
+sentence = input()
+wordsArray = s2w(sentence)
+
+# Print result
+
+for i in wordsArray:
+    print ('-'.join(get_syllables(i))+" ",end="")
