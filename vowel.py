3ee45d3e40fd7e0dc7697be0151b83f0540e63ab
Daniel Kowalski <hal4ed@gmail.com>
2015-06-04 08:39:05 +0200

    Update vowel.py
    Fix lower in vowel.py

--- vowel.py
+++ vowel.py
@@ -1,7 +1,7 @@
 __author__ = 'obir'
 # -*- coding: utf-8 -*-
 
-word = 'Потолок'
+word = 'Юрий'
 vowel_set = ('а','е','ё','и','й','о','у','э','ю','я')
 vowel_set_found = []
 vowel_found_pos = []
@@ -10,7 +10,7 @@ pos = 0
 for anyletter in word:
     pos = pos + 1
     for anyvowel in vowel_set:
-        if anyletter == anyvowel:
+        if anyletter.lower() == anyvowel:
             vowel_set_found.append(anyletter)
             vowel_found_pos.append(pos)
 
