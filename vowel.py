75e169172f5b705003eb0392ba62b49c0c4ed890
Daniel Kowalski <hal4ed@gmail.com>
2015-06-04 08:33:21 +0200

    Update vowel.py
    Fix vowel.py

--- vowel.py
+++ vowel.py
@@ -1,20 +1,24 @@
 __author__ = 'obir'
 # -*- coding: utf-8 -*-
 
-word = u"Гидроэлектростанция"
-print (word)
-
+word = 'Потолок'
 vowel_set = ('а','е','ё','и','й','о','у','э','ю','я')
 vowel_set_found = []
+vowel_found_pos = []
 pos = 0
 
 for anyletter in word:
+    pos = pos + 1
     for anyvowel in vowel_set:
         if anyletter == anyvowel:
             vowel_set_found.append(anyletter)
-            vowel_set_found.append(pos)
+            vowel_found_pos.append(pos)
 
+
+print (word)
 
 print (vowel_set_found)
-print (len(vowel_set_found))
+print (vowel_found_pos)
 
+print ("Length: ",end = "")
+print (len(vowel_set_found))
