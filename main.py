ffd6bbe5247da93659d6dd46a2e570392c9bec63
Daniel Kowalski <hal4ed@gmail.com>
2015-06-09 19:12:40 +0200

    В жопу Юникод

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-# -*- coding: utf-8 -*-
+
 
 from vowel import *
 
 
-#print (findvowels(u'Телевизор'))
-#print (findvowels(u'Городской'))
-
-print (findvowels(u'Гидроэлектростанция'))
-print (x_repl(u'Гидроэлектростанция'))
+print (findvowels('Гидроэлектростанция'))
+print (x_repl('Гидроэлектростанция'))
