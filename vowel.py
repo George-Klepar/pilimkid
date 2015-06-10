de5860866ef61b1d6186c1a8b529e9db040c9f8f
Daniel Kowalski <hal4ed@gmail.com>
2015-06-10 21:05:14 +0200

    Добавлены:
     1) Функция проверки на гласную
     2) Функция конвертации списка в строку
     3) Добавлены 3 правила первода

--- vowel.py
+++ vowel.py
@@ -1,6 +1,7 @@
+vowel_set = ('а', 'е', 'ё', 'и', 'о', 'у', 'э', 'ю', 'я')
+I = 'i'
 
 def findvowels(sword):
-    vowel_set = ('а', 'е', 'ё', 'и', 'о', 'у', 'э', 'ю', 'я')
     vowel_set_found = []
     vowel_found_pos = []
     pos = 0
@@ -14,6 +15,7 @@ def findvowels(sword):
     return vowel_found_pos
 
 
+#меняет все гласные на i
 def x_repl(slovo):
     new_slovo=slovo
     temp_slovo = list(new_slovo)
@@ -21,3 +23,33 @@ def x_repl(slovo):
     for i in fvowels:
         temp_slovo[i-1] = u'i'
     return temp_slovo
+
+#Проверка, гласная ли буква
+def checkifvowel(letter):
+    result = False
+    for element in vowel_set:
+        if letter == element:
+            result = True
+    return result
+
+#Объединяет элементы списка в строку
+def listtostring(input_list):
+    result = ''.join(input_list)
+    return result
+
+
+def azirivka(slovo):
+    temp_slovo = list(slovo)
+    fvowels = findvowels(slovo)
+
+    #тут начинаются правила
+    if len(fvowels)==2 and (checkifvowel(slovo[len(slovo)-1])==True):
+        temp_slovo[fvowels[0]-1] = I
+
+    if len(fvowels)==1:
+        temp_slovo[fvowels[0]-1] = I
+
+    if len(fvowels) == 2 and (checkifvowel(slovo[len(slovo)-1])==False):
+        temp_slovo[fvowels[1]-1] = I
+
+    return listtostring(temp_slovo)
